d0755deaac43346b979ed2632aa431ea3449cf64
atikurrahman33 <foysalatik33@gmail.com>
2023-11-02 21:05:58 +0600

    Add Banner

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import logo from '../../../assets/logo.svg.svg'
 
 const Navbar = () => {
     return (
-        <div className="navbar bg-base-100">
+        <div className="navbar bg-base-100 h-28 mb-4">
             <div className="navbar-start">
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
@@ -12,33 +12,19 @@ const Navbar = () => {
                     </label>
                     <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
                         <li><a>Item 1</a></li>
-                        <li>
-                            <a>Parent</a>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </li>
+                       
                         <li><a>Item 3</a></li>
                     </ul>
                 </div>
-                <Link to={'/'} className="btn btn-ghost normal-case text-xl">
+                <Link to={'/'} className=" normal-case text-xl">
                     <img src={logo} alt="" />
                 </Link>
             </div>
             <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal px-1">
-                    <li><a>Item 1</a></li>
-                    <li tabIndex={0}>
-                        <details>
-                            <summary>Parent</summary>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </details>
-                    </li>
-                    <li><a>Item 3</a></li>
+                <ul className="menu menu-horizontal gap-3 px-1">
+                    <Link>Home</Link>
+                   
+                    <Link>About</Link>
                 </ul>
             </div>
             <div className="navbar-end">
